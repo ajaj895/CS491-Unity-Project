0d485fa254d9ec922c05119090903c584a569b52
Evan Colwell <eccolwell99@gmail.com>
2020-04-07 17:34:29 -0500

    MainMenu.cs documentation update
    Added better documentation for the MainMenu script.

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -1,18 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.SceneManagement;
+using UnityEngine;//Always needed
+using UnityEngine.SceneManagement;//Needed for changing scenes and scene work
 
 public class MainMenu : MonoBehaviour
 {
-    public void StartGame()//starts game (Actually goes to the next scene in the build which is the Game scene)
+    public void StartGame()//Starts game (Actually goes to the next scene in the build which is the Game scene)
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);//Goes to the next scene in the build order (MAKE SURE BUILD ORDER IS CORRECT)
     }
 
-    public void QuitGame()
+    public void QuitGame()//Quits game outside of the unity editor (Note: does not do anything in the editor)
     {
-        Debug.Log("Quiting Game.");
+        Debug.Log("Quiting Game.");//For debug purposes in the editor
         Application.Quit();
     }
 }
