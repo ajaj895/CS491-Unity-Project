304d3cb9769c989c28fb1fae24e21368f8b6e3c3
Evan Colwell <eccolwell99@gmail.com>
2020-04-07 16:21:18 -0500

    Main Phase II commit
    Added the major components of the Phase II portion of the project. Major UI scenes, and scripts.

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void StartGame()//starts game (Actually goes to the next scene in the build which is the Game scene)
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quiting Game.");
+        Application.Quit();
+    }
+}
